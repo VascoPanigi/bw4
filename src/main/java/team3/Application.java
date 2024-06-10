af203e2a0568bda99740b2b06a998aab3051bd55
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-06-10 15:44:43 +0200

    update merge

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -1,12 +1,23 @@
 package team3;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import team3.dao.MembershipDAO;
+import team3.entities.membership.Membership;
+import team3.entities.utils.Suppliers;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        MembershipDAO md = new MembershipDAO(em);
+
+        for (int i = 0; i < 20; i++) {
+            Membership newMembership = Suppliers.membershipSupplier.get();
+            md.save(newMembership);
+            System.out.println("YIPPIEE");
+        }
     }
 }
