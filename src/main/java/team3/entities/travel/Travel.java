53a8d5f557097e1f1905a322ed418fd3b6b41936
Matteo Moscardini <160152042+matte-mosca@users.noreply.github.com>
2024-06-10 15:49:40 +0200

    Travel

--- src/main/java/team3/entities/travel/Travel.java
+++ src/main/java/team3/entities/travel/Travel.java
@@ -1,9 +1,63 @@
 package team3.entities.travel;
+import jakarta.persistence.*;
+import transportation.TransportationType;
 
+@Entity
+@Table(name = "travel")
 public class Travel {
     private int number;
     private int duration;
     private String startingPoint;
     private String terminus;
+    private TransportationType transportationType;
+
+    public Travel (int number, int duration, String startingPoint, String terminus, TransportationType transportationType) {
+        this.number = number;
+        this.duration = duration;
+        this.startingPoint = startingPoint;
+        this.terminus = terminus;
+        this.transportationType = transportationType;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getNumber(){
+        return number;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    @ManyToOne
+    public TransportationType getTransportationType (){
+        return transportationType;
+    }
+
+    public void setTransportationType(TransportationType transportationType) {
+        this.transportationType = transportationType;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public String getStartingPoint(){
+        return startingPoint;
+    }
+
+    public void setStartingPoint(String startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public String getTerminus() {
+        return terminus;
+    }
+
+    public void setTerminus(String terminus) {
+        this.terminus = terminus;
+    }
     
 }
