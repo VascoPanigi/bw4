7af65ad0e90938671f316576b61865fa194f759c
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-06-10 15:46:02 +0200

    commit distributor, transportation and ticket

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -7,6 +7,7 @@ public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4");
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/team3/user/User.java
@@ -0,0 +1,12 @@
+package team3.user;
+
+
+
+public class User {
+
+    private String name;
+
+    private String surname;
+
+//private Card card;
+}
